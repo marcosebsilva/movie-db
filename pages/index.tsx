a42303df5f97bfecab4f7ea727b8e568c9aefb75
Marcos Estevao <dusdoom.dev@gmail.com>
2022-02-28 17:58:15 -0300

    feat: :sparkles: added static props to carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
 import type { NextPage } from 'next';
 import Header from 'components/Header';
+import Carousel from 'components/Carousel';
+import { TRENDING, RANDOM_MOVIES } from 'utils/dictionary/api_urls.json';
+import { getShows } from 'axios/TMDB/calls';
+import { MovieObject } from 'types'
 
-const Home: NextPage = () => {
+interface Props {
+  trending: Array<MovieObject>,
+  allMovies: Array<MovieObject>,
+}
+
+const Home: NextPage<Props> = ({trending}) => {
   return (
-    <div>
+    <>
       <Header />
-      Home
-    </div>)
+      <Carousel trending={ trending }/>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const randomShows = await getShows(RANDOM_MOVIES, 10);
+  const trending = await getShows(TRENDING, 5);
+
+  return {
+    props: {
+      trending,
+      randomShows,
+    }
+  }
 }
 
 export default Home;
